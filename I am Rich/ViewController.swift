845e711fdb128037a918f6ea445c3e2258c4115a
Will Frost <williamf1245@gmail.com>
2020-07-22 17:56:25 +0200

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by William Frost on 2020/07/22.
+//  Copyright © 2020 William Frost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
